918f59d4aa4018348fb7c2506cde7fa33b56e179
samibegg <samibegg@samis-mbp.lan>
2025-05-01 11:56:29 -0400

    sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,31 @@
 module.exports = {
   siteUrl: 'https://forgemission.com',
   generateRobotsTxt: true,
+  exclude: [
+    '/knowledge/AI-Strategy-2025-Policies-Competitive-Analysis',
+    '/knowledge/AI-Strategy-2025-Policies-Funding',
+    '/knowledge/AI-Strategy-2025-Policies-Key-Challenges',
+    '/knowledge/AI-Strategy-2025-Policies-Landscape',
+    '/knowledge/AI-Strategy-2025-Policies-Opportunities',
+    '/knowledge/AI-Strategy-2025-Policies-Recommendations',
+    '/knowledge/AI-Strategy-2025-Policies-References',
+    '/knowledge/AI-Strategy-2025-Policies-Sectoral-Impacts',
+  ],
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        disallow: [
+          '/knowledge/AI-Strategy-2025-Policies-Competitive-Analysis',
+          '/knowledge/AI-Strategy-2025-Policies-Funding',
+          '/knowledge/AI-Strategy-2025-Policies-Key-Challenges',
+          '/knowledge/AI-Strategy-2025-Policies-Landscape',
+          '/knowledge/AI-Strategy-2025-Policies-Opportunities',
+          '/knowledge/AI-Strategy-2025-Policies-Recommendations',
+          '/knowledge/AI-Strategy-2025-Policies-References',
+          '/knowledge/AI-Strategy-2025-Policies-Sectoral-Impacts',
+        ],
+      },
+    ],
+  },
 };
